98c1824d06872c84aba142de31e6ed2f5d3ca3d9
ASUS <ASUS@DESKTOP-G258SE4>
2018-11-11 19:12:56 +0700

    Class Fungsi Mahasiswa

---
+++ Tugas Pengganti/src/tugas/pengganti/mahasiswa.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas.pengganti;
+
+/**
+ *
+ * @author ASUS
+ */
+class Mahasiswa {
+     private String nim;
+    private String nama;
+    private double ipk;
+    private int ind;
+    
+    public Mahasiswa(){
+        this.nim = nim;
+        this.nama = nama;
+        this.ipk = ipk;
+        this.ind = ind;
+        
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public double getIpk() {
+        return ipk;
+    }
+
+    public void setIpk(double ipk) {
+        this.ipk = ipk;
+    }
+
+    public int getInd() {
+        return ind;
+    }
+
+    public void setInd(int ind) {
+        this.ind = ind;
+    }
+}
+    
+
